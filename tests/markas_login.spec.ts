0eb5200c5579a07748cba2bd7fa2b0cae2f54886
Ika Nur Cahya S <inuryka22@gmail.com>
2024-11-11 18:12:08 +0700

    Initial commit

---
+++ tests/markas_login.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+import * as fs from 'fs'; 
+import * as path from 'path';
+const filePath: string = path.join(__dirname, 'selector.json');
+let selectors;
+try { 
+  const data: string = fs.readFileSync(filePath, 'utf8'); 
+  selectors = JSON.parse(data); console.log('File content:', selectors); 
+} 
+  catch (err) { 
+    console.error('Error reading file:', (err as NodeJS.ErrnoException).message); 
+  }
+test('@test_login_markas', async ({ page }) => {
+  await page.goto('https://markas.staging.belajar.id/login');
+  await page.locator(selectors.login_page.masukkan_email).click();
+  await page.locator(selectors.login_page.masukkan_email).fill('skm@kemdikbud.go.id');
+  await page.locator(selectors.login_page.masukkan_password).click();
+  await page.locator(selectors.login_page.masukkan_password).fill('Default#1234!');
+  await page.locator(selectors.login_page.dropdown_tahun_anggaran).click();
+  await page.locator(selectors.login_page.dropdown_tahun_anggaran.tahun_anggaran_2021).click();
+});
---
+++ tests/test-4.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('nces1', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.pause()
+  await expect(page.locator('[data-test="item-4-title-link"] [data-test="inventory-item-name"]')).toContainText('Sauce Labs Backpack');
+  await expect(page.locator('[data-test="shopping-cart-link"]')).toBeVisible();
+  
+});
