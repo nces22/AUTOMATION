476acab0bd7a7f27dd8e21068db7f2bd302650e1
Ika Nur Cahya S <inuryka22@gmail.com>
2024-11-11 18:18:35 +0700

    SDSADM-0001 dropdown tahun anggaran

--- tests/markas_login.spec.ts
+++ tests/markas_login.spec.ts
@@ -18,4 +18,5 @@ test('@test_login_markas', async ({ page }) => {
   await page.locator(selectors.login_page.masukkan_password).fill('Default#1234!');
   await page.locator(selectors.login_page.dropdown_tahun_anggaran).click();
   await page.locator(selectors.login_page.dropdown_tahun_anggaran.tahun_anggaran_2021).click();
+  await page.locator(selectors.login_page.dropdown_tahun_anggaran.tahun_anggaran_2022).click();
 });
